dab4ed3c60ad55edd07c8eba380615920bd96856
capsukawa <capsukawa@gmail.com>
2016-02-01 10:01:00 +0100

    Debut SpaceInvaders moisi mais voila ok cool bien

--- game.py
+++ game.py
@@ -3,69 +3,78 @@ import pygame
 import os,inspect
 
 def load_image(name):
-    name = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+name
+    name = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+'/'+name
     image = pygame.image.load(name)
     return image
 
 pygame.init()
 
-background_image = load_image("/tutos/les_evenements/background.jpg")
-background_position = [0, 0]
-
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 size = width, height = 640, 480
 screen = pygame.display.set_mode(size)
 
-speed = [1, 1]
-bspeed = [0,-5]
-invader = load_image("/tutos/les_evenements/InvaderA_00.png")
-invader_rect = invader.get_rect()
-my_hero = load_image("/tutos/les_evenements/InvaderB_00.png")
-my_hero_rect = my_hero.get_rect()
-bstack1 = []
-bstack2 = []
+background = load_image("background.png")
+background_position = [0,0]
+
+hero = load_image("hero.png")
+hero_rect = hero.get_rect()
+hero_rect.left = 296
+hero_rect.top = 430
+
+bullet = load_image("bullet.png")
+bullet_rects = []
+bullet_time=0
+
+enemy = load_image("enemy.png")
+enemy_rects = []
+enemy_alive = []
+for i in range(0,5):
+	te = enemy.get_rect()
+	te.left = i*60 + 2
+	enemy_rects.append(te)
+	enemy_alive.append(1)
 
 while 1:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 
-	invader_rect = invader_rect.move(speed)
-
-	if invader_rect.left < 0 or invader_rect.right > width:
-		speed[0] = -speed[0]
-
-	if invader_rect.top < 0 or invader_rect.bottom > height:
-		speed[1] = -speed[1]
-
+	# Evenements clavier
 	keys = pygame.key.get_pressed()
 
-	if keys[pygame.K_LEFT] and my_hero_rect.left > 0:
-		my_hero_rect = my_hero_rect.move(-5, 0)
+	if keys[pygame.K_LEFT] and hero_rect.left > 5:
+		hero_rect = hero_rect.move(-3,0)
 
-	if keys[pygame.K_RIGHT] and my_hero_rect.right < width:
-		my_hero_rect = my_hero_rect.move(5, 0)
+	if keys[pygame.K_RIGHT] and hero_rect.right < width-5:
+		hero_rect = hero_rect.move(3,0)
 
-	if keys[pygame.K_UP] and my_hero_rect.top > 0:
-		my_hero_rect = my_hero_rect.move(0, -5)
+	if keys[pygame.K_SPACE] and bullet_time<=0:
+		bullet_time = 20
+		tb = bullet.get_rect()
+		tb.left = (hero_rect.right+hero_rect.left)/2
+		tb.top = hero_rect.top-2
+		bullet_rects.append(tb)
 
-	if keys[pygame.K_DOWN] and my_hero_rect.bottom < height:
-		my_hero_rect = my_hero_rect.move(0, 5)
+	# Gros bordel
+	bullet_time-=1
+	screen.blit(background,background_position)
+	screen.blit(hero,hero_rect)
 
-	if keys[pygame.K_SPACE]:
-		bstack1.append(load_image("/bullet.png"))
-		if bstack1:
-			bstack2.append(bstack1[len(bstack1)-1].get_rect())
-			bstack2[len(bstack2)-1] = my_hero_rect
-			print(bstack2)
-			for item in bstack2:
-				item = item.move(0,-5)
-
-	screen.blit(background_image, background_position)
-	screen.blit(invader, invader_rect)
-	screen.blit(my_hero, my_hero_rect)
-	for item in bstack2:
-		screen.blit(bstack1[0],item)
+	bullet_vdb = 0
+	for i in bullet_rects:
+		if i.bottom<0:
+			bullet_rects.pop(bullet_vdb)
+		enemy_vdb = 0
+		for y in enemy_rects:
+			if i.top<y.bottom and i.centerx<y.right and i.centerx>y.left:
+				bullet_rects.pop(bullet_vdb)
+				enemy_rects.pop(enemy_vdb)
+			enemy_vdb+=1
+		i.top = i.top-10
+		screen.blit(bullet,i)
+		bullet_vdb+=1
+	for y in enemy_rects:
+		screen.blit(enemy,y)
 
 	pygame.display.flip()
 	screen.blit
