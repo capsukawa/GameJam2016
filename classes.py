904447d9a808c0a4f9c567e3910c5005a7c01d46
capsukawa <capsukawa@gmail.com>
2016-02-02 21:55:37 +0100

    on ira pas en finale

--- classes.py
+++ classes.py
@@ -2,10 +2,11 @@ import sys
 import pygame
 import os
 
-class Hero:
-	viePleine = 2
-	vieCourante = 2
-	puissance = 1
-	vitesseTir = 1
-	vitesseDepl = 2
-	nbProjectiles = 1
+class Hero():
+	def __init__(self):
+		self.viePleine = 2
+		self.vieCourante = 2
+		self.puissance = 1
+		self.vitesseTir = 1
+		self.vitesseDepl = 2
+		self.nbProjectiles = 1
