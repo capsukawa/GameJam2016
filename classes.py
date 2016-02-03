825f61eb60048d5a734f4b48a52891bf8cafc438
Catka <cetra38@hotmail.fr>
2016-02-03 10:57:24 +0100

    git c'est nul
    classes boss

--- classes.py
+++ classes.py
@@ -3,7 +3,15 @@ import pygame
 import os
 import util
 
-boss1 = [10,1,"boss/araignee.png"]
+boss1 = [50,1,"boss/araignee.png"]
+boss2 = [100,1,"boss/rat.png"]
+boss3 = [150,2,"boss/fantome.png"]
+boss4 = [300,2,"boss/squelette.png"]
+boss5 = [1000,4,"boss/troll.png"]
+boss6 = [700,5,"boss/mage.png"]
+boss7 = [1000,10,"boss/vampire.png"]
+boss8 = [1500,15,"boss/dragon1.png"]
+boss9 = [2000,20,"boss/dragon3.png"]
 
 class Hero():
 	def __init__(self):
